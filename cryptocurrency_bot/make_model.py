16edafec965c4df0a7b6077b4978fef9451324a9
blancaile <yuuheimtg@icloud.com>
2021-12-04 21:34:28 +0900

    特徴量と教師データ作成関数

--- cryptocurrency_bot/make_model.py
+++ cryptocurrency_bot/make_model.py
@@ -22,13 +22,38 @@ def make_feature(df):
     return df.dropna(how="any")
 
 
+
+#up,stay,downの閾値とxを受け取りx分後のethのcloseの変化率を３値(1,0,-1)に分類する
+def make_training_data(df, x = 1, up = 0.0001, down = -0.0001):
+    e_d = df.to_dict()
+
+    for i in range(len(e_d) - x):
+        ratio = (e_d[i+x] - e_d[i]) / e_d[i]
+        #print("ratio is ", ratio)
+        if ratio > up:
+            e_d[i] = 1
+        elif ratio < down:
+            e_d[i] = -1
+        else:
+            e_d[i] = 0
+
+    #print(type(e_d))
+    #eth_ratio = pd.DataFrame(e_d, index=["i",]) #遅い
+    eth_ratio = pd.DataFrame.from_dict(e_d, orient="index")
+    eth_ratio.iloc[-x:] = np.nan #後ろからx分はNaN
+    #print(eth_ratio)
+    return eth_ratio
+
+
 BUF = pd.read_csv(r"C:\github_folder\study_machine_learning\cryptocurrency_bot\datasets\edit_btcusdt_f.csv")
 EUF = pd.read_csv(r"C:\github_folder\study_machine_learning\cryptocurrency_bot\datasets\edit_ethusdt_f.csv")
 
 
 BUF_feature = make_feature(BUF)
 EUF_feature = make_feature(EUF)
+train = make_training_data(EUF["close"], 1, 0.0001, -0.0001)
 
+print(train)
 #結合
 feature = pd.concat([BUF_feature, EUF_feature], axis=1)
 
