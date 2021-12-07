ff21e763b3d2f3a2b514f42e0f1325313dc361c9
blancaile <yuuheimtg@icloud.com>
2021-12-07 10:49:00 +0900

    sliding windowとshape調整

--- cryptocurrency_bot/make_model.py
+++ cryptocurrency_bot/make_model.py
@@ -12,21 +12,13 @@ import tensorflow_datasets as tfds
 from sklearn.preprocessing import StandardScaler
 import scipy.stats
 
-def shaping():
-    # open     high      low    close  Volume ETH   Volume USDT  tradecount
-    ethusdtf = pd.read_csv(r"C:\github_folder\study_machine_learning\cryptocurrency_bot\datasets\ethusdt_f.csv")
-    ethusdtfsort = ethusdtf.sort_values("unix")
-    ethusdtfsort = ethusdtfsort.reset_index(drop=True)
-    ethusdtfsort.drop(["unix", "date", "symbol"], axis=1, inplace=True)
-    ethusdtfsort.to_csv("edit_ethusdt_f.csv")
-
-
 
 
 #受け取ったデータから特徴量を生成
 def make_feature(df):
     df["rsi9"] = ta.RSI(df["close"], timeperiod=9)
-    return df.dropna(how="any")
+    #return df.dropna(how="any") #後でnanの個数を使う
+    return df
 
 
 
@@ -50,55 +42,83 @@ def make_training_data(ed, x = 1, up = 0.0001, down = -0.0001):
     eth_ratio = pd.DataFrame.from_dict(e_d, orient="index")
     eth_ratio.iloc[-x:] = np.nan #後ろからx分はNaN
     #print(eth_ratio)
-    return eth_ratio
-
-#1日分の
-def slided_view2D(arr, N=1440):
-    r, c = arr.shape
-    rs, cs = arr.strides
-    strided = np.lib.stride_tricks.as_strided
-    return strided(arr, shape=(r-N+1, N, c), strides=(rs, rs, cs))
+    return eth_ratio.dropna(how="any")
 
 
-#読み込み
+
+#sliding windowを生成
+#for i in range(window_size, len(feature)):  #重い遅い
+#    data.append(feature[i - window_size: i, :])
+#    #print(type(data))
+
+
+#https://stackoverflow.com/questions/43185589/sliding-windows-from-2d-array-that-slides-along-axis-0-or-rows-to-give-a-3d-arra
+def strided_axis0(a, L): 
+    # INPUTS :
+    # a is array
+    # L is length of array along axis=0 to be cut for forming each subarray
+
+    # Length of 3D output array along its axis=0
+    nd0 = a.shape[0] - L + 1
+
+    # Store shape and strides info
+    m,n = a.shape
+    s0,s1 = a.strides
+
+    # Finally use strides to get the 3D array view
+    return np.lib.stride_tricks.as_strided(a, shape=(nd0,L,n), strides=(s0,s0,s1))
+
+
+
+
+#ファイルの読み込み
 BUF = pd.read_csv(r"C:\github_folder\study_machine_learning\cryptocurrency_bot\datasets\edit_btcusdt_f.csv")
 EUF = pd.read_csv(r"C:\github_folder\study_machine_learning\cryptocurrency_bot\datasets\edit_ethusdt_f.csv")
 
-print("hello")
-#説明変数を作成
+
+#説明変数を生成
 BUF_feature = make_feature(BUF)
 EUF_feature = make_feature(EUF)
 
-window_size = 1440
 
-#目的変数を作成
-train= make_training_data(EUF["close"], 1, 0.0001, -0.0001)
-
+#目的変数を生成
+mlater = 1 #何分後のup,downを予測するか
+train= make_training_data(EUF["close"], mlater, 0.0001, -0.0001)
+train = train.to_numpy()
 
 
 #説明変数の結合
 feature = pd.concat([BUF_feature, EUF_feature], axis=1)
+nancount = len(feature[feature.isnull().any(axis=1)])
+
+#print(nancount)
+feature = feature.dropna(how="any").to_numpy()
+
+
+#目的変数データ生成時にできたnanを含む行を説明変数データから削除
+feature = np.delete(feature, [i for i in range(len(feature) - mlater, len(feature))], axis=0)
 
 
 #標準化
-feature = feature.to_numpy()
 feature = scipy.stats.zscore(feature)
+#print(feature.shape)
 
 
 #ウィンドウ作成
-data = []
 window_size = 1440
 
 
-
-for i in range(window_size, len(feature)):  #重い遅い
-    data.append(feature[i - window_size: i, :])
-    #print(type(data))
-
-
-slide_feature = np.array(data)
-print(slide_feature)
+slide_feature = strided_axis0(feature, window_size)
+#print(slide_feature)
 print(slide_feature.shape)
+
+
+
+#教師データと訓練データのサイズ調整
+train = np.delete(train, [i for i in range(window_size + nancount -1)], axis=0)
+#print("////////////////")
+#print(train)
+print(train.shape)
 
 
 
